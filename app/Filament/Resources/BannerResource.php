5eaa65a4aa082fd769fed68590f2677b3077c351
Prashantdhepe <prashantdhepe569@gmail.com>
2026-01-14 12:04:20 +0530

    minor changes

--- app/Filament/Resources/BannerResource.php
+++ app/Filament/Resources/BannerResource.php
@@ -19,6 +19,7 @@ use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class BannerResource extends Resource
 {
